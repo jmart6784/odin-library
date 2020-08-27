1d41def35c637384affcd76c7605eef65e9a01c5
Juan <jmartinez6784@yahoo.com>
2020-08-26 22:59:40 -0700

    local storage support

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let library = [];
+
+// localStorage.removeItem("library");
+
 let newBtn = document.getElementById("nb-btn");
 let popUp = document.getElementById("pu-container");
 let closePU = document.getElementById("close");
@@ -16,7 +19,8 @@ closePU.addEventListener("click", function() {
 });
 
 // Flash error msg
-function flashError() {
+function flashError(msg) {
+  errorMsg.textContent = msg;
   errorMsg.style.display = "block";
 
   setTimeout(function() {
@@ -43,136 +47,156 @@ function Book(title, author, pages, read) {
 
 // Add Book to library
 function addBook(book) {
+  // Temporay Array for holding old books
+  let tempAry = [];
+
+  // Put old books from local storage in temporary array
+  if (localStorage.getItem("library") !== null) {
+    JSON.parse(localStorage.getItem("library")).forEach( b => {
+      tempAry.push(b);
+    });
+  };
+
+  // Put old books into library array
+  tempAry.forEach( b => {
+    library.push(b);
+  });
+
+  // put new book into library
   library.push(book);
-};
 
-let book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
-let book2 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "336", true);
-let book3 = new Book("The Lion, the Witch and the Wardrobe", "C. S. Lewis", "445", false);
+  // Save library to local storage
+  localStorage.setItem("library", JSON.stringify(library));
 
-addBook(book1);
-addBook(book2);
-addBook(book3);
-
-function deleteBook(id) {
-  
+  // Reset both arrays
+  tempAry = [];
+  library = [];
 };
 
 function render() {
-  // Remove all previous renders so there are no accidental duplicates
-  document.querySelectorAll(".book-div").forEach( book => book.remove() )
-
-  let id = 0;
-  library.forEach(book => {
-    let container = document.getElementById("container");
-
-    // Create Div for every book
-    let bookDiv = document.createElement("div");
-    bookDiv.className = "book-div";
-    bookDiv.id = `book${id}`;
-
-    container.appendChild(bookDiv);
-
-    // Create title
-    let titleTag = document.createElement("h2");
-    let titleText = document.createTextNode(book.title);
-
-    titleTag.className = "book-title";
-
-    titleTag.appendChild(titleText);
-
-    bookDiv.appendChild(titleTag);
-
-    // Create author
-    let authorTag = document.createElement("p");
-    let authorText = document.createTextNode(`By ${book.author}`);
-
-    authorTag.className = "info-text";
-
-    authorTag.appendChild(authorText);
-
-    bookDiv.appendChild(authorTag);
-
-    // Create pages
-    let pagesTag = document.createElement("p");
-    let pagesText = document.createTextNode(`${book.pages} pages long`);
-
-    pagesTag.className = "info-text";
-
-    pagesTag.appendChild(pagesText);
-
-    bookDiv.appendChild(pagesTag);
-
-    // Create read
-    let readTag = document.createElement("p");
-    let readText = document.createTextNode(`Read: ${book.read}`);
-
-    readTag.className = "info-text";
-
-    readTag.appendChild(readText);
-
-    bookDiv.appendChild(readTag);
-
-    // Create read or not read button
-    let btnTxt = "";
-    let readBtn = document.createElement("button");
-
-    if (book.read === true) {
-      btnTxt = "Read"
-      readBtn.className = "read-btn";
-    } else {
-      btnTxt = "Not Read"
-      readBtn.className = "not-read-btn";
-    };
-
-    let rBtnText = document.createTextNode(btnTxt);
   
-    readBtn.appendChild(rBtnText);
+  if (localStorage.getItem("library") !== null) {
+    // Remove all previous renders so there are no accidental duplicates
+    document.querySelectorAll(".book-div").forEach( book => book.remove() )
 
-    bookDiv.appendChild(readBtn);
+    let id = 0;
 
-    // Change Read state and render
-    readBtn.onclick = function() {
-      if (book.read === true) {
-        book.read = false;
-        render();
+    JSON.parse(localStorage.getItem("library")).forEach(book => {
+      // let desBook = JSON.parse(book);
+      let desBook = book;
+
+      let container = document.getElementById("container");
+
+      // Create Div for every book
+      let bookDiv = document.createElement("div");
+      bookDiv.className = "book-div";
+      bookDiv.id = `book${id}`;
+
+      container.appendChild(bookDiv);
+
+      // Create title
+      let titleTag = document.createElement("h2");
+      let titleText = document.createTextNode(desBook.title);
+
+      titleTag.className = "book-title";
+
+      titleTag.appendChild(titleText);
+
+      bookDiv.appendChild(titleTag);
+
+      // Create author
+      let authorTag = document.createElement("p");
+      let authorText = document.createTextNode(`By ${desBook.author}`);
+
+      authorTag.className = "info-text";
+
+      authorTag.appendChild(authorText);
+
+      bookDiv.appendChild(authorTag);
+
+      // Create pages
+      let pagesTag = document.createElement("p");
+      let pagesText = document.createTextNode(`${desBook.pages} pages long`);
+
+      pagesTag.className = "info-text";
+
+      pagesTag.appendChild(pagesText);
+
+      bookDiv.appendChild(pagesTag);
+
+      // Create read
+      let readTag = document.createElement("p");
+      let readText = document.createTextNode(`Read: ${desBook.read}`);
+
+      readTag.className = "info-text";
+
+      readTag.appendChild(readText);
+
+      bookDiv.appendChild(readTag);
+
+      // Create read or not read button
+      let btnTxt = "";
+      let readBtn = document.createElement("button");
+
+      if (desBook.read === true) {
+        btnTxt = "Read"
+        readBtn.className = "read-btn";
       } else {
-        book.read = true;
+        btnTxt = "Not Read"
+        readBtn.className = "not-read-btn";
+      };
+
+      let rBtnText = document.createTextNode(btnTxt);
+    
+      readBtn.appendChild(rBtnText);
+
+      bookDiv.appendChild(readBtn);
+
+      // Change Read state and render
+      readBtn.onclick = function() {
+        if (desBook.read === true) {
+          desBook.read = false;
+          render();
+        } else {
+          desBook.read = true;
+          render();
+        };
+      };
+
+      // Add Delete button
+      Array.prototype.remove = function(value) {
+        this.splice(this.indexOf(value), 1);
+      };
+
+      let deleteTag = document.createElement("button");
+      let deleteText = document.createTextNode(`Remove`);
+
+      deleteTag.className = "rm-book";
+
+      deleteTag.onclick = function() {
+        library.remove(desBook);
         render();
       };
-    };
 
-    // Add Delete button
-    Array.prototype.remove = function(value) {
-      this.splice(this.indexOf(value), 1);
-    };
+      deleteTag.appendChild(deleteText);
 
-    let deleteTag = document.createElement("button");
-    let deleteText = document.createTextNode(`Remove`);
+      bookDiv.appendChild(deleteTag);
 
-    deleteTag.className = "rm-book";
+      // ID
+      let idTag = document.createElement("p");
+      let idText = document.createTextNode(`ID: ${id}`);
 
-    deleteTag.onclick = function() {
-      library.remove(book);
-      render();
-    };
+      idTag.className = "info-text";
 
-    deleteTag.appendChild(deleteText);
+      idTag.appendChild(idText);
 
-    bookDiv.appendChild(deleteTag);
-
-    // ID
-    let idTag = document.createElement("p");
-    let idText = document.createTextNode(`ID: ${id}`);
-
-    idTag.className = "info-text";
-
-    idTag.appendChild(idText);
-
-    bookDiv.appendChild(idTag);
-    
-    id++;
-  });
+      bookDiv.appendChild(idTag);
+      
+      id++;
+    });
+  } else {
+    console.log("EMPTY LIBRARY");
+  };
 };
 
 render();
@@ -184,11 +208,11 @@ submitBtn.addEventListener("click", function() {
   let read1 = document.getElementById("read-input").checked;
 
   if (title1 !== "" && author1 !== "" && pages1 !== "") {
-    book = new Book(title1, author1, pages1, read1)
+    book = new Book(title1, author1, pages1, read1);
+
     addBook(book);
     render();
   } else {
-    console.log("EMPTY");
-    flashError();
+    flashError("Error: One or more fields are empty");
   };
 });
